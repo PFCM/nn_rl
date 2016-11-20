f5bfebccf46fb7aafbda3ca195d7257451cfaefa
Paul Mathews <pfcmathews@gmail.com>
2016-11-21 10:30:20 +1300

    framework for recording

--- play.py
+++ play.py
@@ -7,16 +7,35 @@ import gym
 import players
 
 
+def record_transition(current_state, action, reward, next_state, terminal):
+    """Records a transition into the replay buffer (this probably means
+    waiting for enough of them and then writing to disk).
+
+    Args:
+        current_state: the starting state for the transition.
+        action: the action taken by the agent.
+        reward: the reward associated with this state/action pair.
+        next_state: the subsequent state the world transitioned to.
+        terminal: if the state turned out to be terminal.
+    """
+    logging.info('transition:')
+    logging.info('  current_state: %s', current_state)
+    logging.info('         action: %s', action)
+    logging.info('         reward: %s', reward)
+    logging.info('     next_state: %s', next_state)
+
+
 def play_loop(env):
     """play for a while"""
     logging.info('getting player')
     with players.nn_player('/tmp/rl', 'mlp', env) as player:
-        obs = env.reset()
+        current_state = env.reset()
         for episode in range(1000):
             env.render()
-            action = player(obs)
-            print(action, obs)
-            obs, reward, done, info = env.step(action)
+            action = player(current_state)
+            next_state, reward, done, info = env.step(action)
+            # maybe record the transition
+            record_transition(current_state, action, reward, next_state, done)
 
 
 def main():
