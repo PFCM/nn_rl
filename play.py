901f3a6ab1f6b0c573f9b7aad67c970f0007af47
Paul Mathews <pfcmathews@gmail.com>
2016-12-05 19:30:23 +1300

    make replay buffer a sub package of players

--- play.py
+++ play.py
@@ -7,27 +7,6 @@ import gym
 import players
 
 
-def record_transition(current_state, action, reward, next_state, terminal):
-    """Records a transition into the replay buffer (this probably means
-    waiting for enough of them and then writing to disk).
-
-    Args:
-        current_state: the starting state for the transition.
-        action: the action taken by the agent.
-        reward: the reward associated with this state/action pair.
-        next_state: the subsequent state the world transitioned to.
-        terminal: if the state turned out to be terminal.
-    """
-    logging.debug('transition:')
-    logging.debug('  current_state: %s', current_state)
-    logging.debug('         action: %s', action)
-    logging.debug('         reward: %s', reward)
-    logging.debug('     next_state: %s', next_state)
-
-    players.replay_buffer.store(current_state, action, reward, next_state,
-                                terminal)
-
-
 def play_loop(env):
     """play for a while"""
     logging.info('getting player')
@@ -35,15 +14,22 @@ def play_loop(env):
         current_state = env.reset()
         for episode in range(1000):
             env.render()
-            action = player(current_state)
+            action = player.act(current_state)
             next_state, reward, done, info = env.step(action)
-            # maybe record the transition
-            record_transition(current_state, action, reward, next_state, done)
+            player.reward(reward)
+
+            logging.debug('transition:')
+            logging.debug('  current_state: %s', current_state)
+            logging.debug('         action: %s', action)
+            logging.debug('         reward: %s', reward)
+            logging.debug('     next_state: %s', next_state)
+
             current_state = next_state
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+    gym.logger.setLevel(logging.INFO)
     env = gym.make('Pong-ram-v0')
     env.reset()
 
--- players/nn_player.py
+++ players/nn_player.py
@@ -1,11 +1,13 @@
 """Contains some things we can use to play a game using neural networks.
 """
 import logging
-from contextlib import contextmanager
 
 import tensorflow as tf
 
 import players.nets as nets
+
+
+USE_DEFAULT = 0
 
 
 @contextmanager
@@ -36,3 +38,31 @@ def nn_player(model_dir, model, env, save_summaries=False):
             return sess.run(action_var, {input_var: obs.reshape((1, -1))})
 
         yield act
+
+
+class NNPlayer(object):
+    """Neural network player, loads up a net and maybe remembers how it's been
+    going"""
+
+    def __init__(self, model_dir, model, env, trajectory_saver=USE_DEFAULT):
+        """Makes a new player.
+
+        Args:
+            model_dir (string): where the checkpoints to load are
+            model (string): which model to use. For options, see `nets.py`.
+            env (Environment): the gym environment in which we are to operate.
+            trajectory_saver (Optional): something we can use to save
+                transitions as we observe them. If None, transitions are not
+                saved, if 0 then a default ReplayBuffer is created.
+        """
+        self.input_var = nets.get_input_for(env, 1)
+        self.action_var = tf.squeeze(nets.get_net(model, input_var, env))
+
+
+    def act(self, observation, session):
+        """act on an observation"""
+        pass
+
+    def reward(self, reward):
+        """receive a reward for the last executed action"""
+        pass
